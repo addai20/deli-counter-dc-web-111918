b00de02d9daf2d5ce3d963ab08535663a20595ec
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 02:50:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,14 @@
+# there are people in line
+#       should display the current line (FAILED - 1)
+#       works with different people in line (FAILED - 2)
+#   #take_a_number
+#     there is nobody in line
+#       should add a person to the line (FAILED - 3)
+#     there are already people in line
+#       should add a person to the end of the line (FAILED - 4)
+#     adding multiple people in a row
+
+
 def line(katz_deli)
   statement = "The line is currently: "
   
