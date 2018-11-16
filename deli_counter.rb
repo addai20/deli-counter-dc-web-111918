7c4b4da0a76be33fec0394f9dfa0d50179a8425a
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 03:07:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,6 +27,7 @@ end
 def take_a_number(katz_deli, name)
   if katz_deli.length == 0 
     katz_deli << name
+    puts "Welcome, #{name}. You are number 1 in line."
   elsif katz_deli.length > 0 
     katz_deli.push(name)
   end
