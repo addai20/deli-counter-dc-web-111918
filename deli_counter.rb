589d25689a388ef539542652e3603a142b2d5b76
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 02:51:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,14 +10,13 @@
 
 
 def line(katz_deli)
-  statement = "The line is currently: "
-  
+  statement = "The line is currently:"
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
   
   katz_deli.each_with_index do |person, idx|
-    statement += "#{idx + 1}. #{person} "
+    statement += " #{idx + 1}. #{person}"
   end
   return statement
 end
