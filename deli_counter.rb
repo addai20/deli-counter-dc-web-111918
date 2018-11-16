d0ac6e3fb649ed0de387eb934eea2773c4a6ff52
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 02:43:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  statement = "The line is currently: "
+  
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+  
+  katz_deli.each_with_index do |person, idx|
+    statement += "#{idx + 1}. #{person} "
+  end
+  return statement
+end
+
+def take_a_number
+  
+end
