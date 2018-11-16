c3eb873c54642377798ac2930ade243bf12ba765
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-16 03:29:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -39,5 +39,5 @@ def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length != 0
-    katz_deli.unshift
+    puts katz_deli.unshift
 end
